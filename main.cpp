90df7fbc82a9b853bcf0ca85c206fa3f9bde63bb
KmonIKE <henrique_parro@yahoo.com>
2024-05-07 18:26:36 -0300

    main created

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Henrique on 07/05/2024.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by Henrique on 07/05/2024.
+//
+
+#ifndef COMPILADORES_MAIN_H
+#define COMPILADORES_MAIN_H
+
+#endif //COMPILADORES_MAIN_H
